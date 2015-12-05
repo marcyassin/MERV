9e68cd3f73ca660155ab954c2d8670750ac32af1
Marc Yassin <marc@yass.in>
2015-12-05 15:45:39 -0500

    michael be patient

--- app/src/main/java/cayenne/team/merv/SignUpActivity.java
+++ app/src/main/java/cayenne/team/merv/SignUpActivity.java
@@ -10,64 +10,53 @@ import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
     protected EditText mUsername;
     protected EditText mPassword;
-    protected Button mLoginButton;
-    protected TextView mSignUpTextView;
-
+    protected EditText mEmail;
+    protected Button mSignUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS); // must be before setContentView
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        mSignUpTextView = (TextView)findViewById(R.id.signUpTextView);
-        mSignUpTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
-                startActivity(intent);
-            }
-        });
-        mUsername = (EditText) findViewById(R.id.usernameEditText);
-        mPassword = (EditText) findViewById(R.id.passwordEditText);
-        mLoginButton = (Button) findViewById(R.id.loginButton);
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+        setContentView(R.layout.activity_sign_up);
 
-        mLoginButton.setOnClickListener(new View.OnClickListener() {
+        mUsername = (EditText) findViewById(R.id.signUpUsernameEditText);
+        mPassword = (EditText) findViewById(R.id.signUpPasswordEditText);
+        mEmail = (EditText) findViewById(R.id.signUpEmailEditText);
+        mSignUpButton = (Button) findViewById(R.id.signUpButton);
+
+        mSignUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String username = mUsername.getText().toString();
                 String password = mPassword.getText().toString();
+                String email = mEmail.getText().toString();
+
                 username = username.trim();
                 password = password.trim();
+                email = email.trim();
 
-                if (username.isEmpty() || password.isEmpty()) {
-                    AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
-                    builder.setMessage(R.string.login_error_message)
-                            .setTitle(R.string.login_error_title)
+                if (username.isEmpty() || password.isEmpty() || email.isEmpty()) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
+                    builder.setMessage(R.string.signup_error_message)
+                            .setTitle(R.string.signup_error_title)
                             .setPositiveButton(android.R.string.ok, null);
 
                     AlertDialog dialog = builder.create();
                     dialog.show();
                 } else {
-                    // Login flow
 
-                    setProgressBarIndeterminateVisibility(true);
-
-
-
+                    // code for Signup goes here
                 }
 
             }
         });
-
-
-
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
