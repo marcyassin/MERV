63b7b8b01bc2c3526a43d94b0b58d01745bb2b04
trankmichael <trankmichael@gmail.com>
2015-12-05 19:55:12 -0500

    - added task deletion and addition in listview

--- app/src/main/java/cayenne/team/merv/TabFragment1.java
+++ app/src/main/java/cayenne/team/merv/TabFragment1.java
@@ -7,13 +7,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import com.firebase.client.ChildEventListener;
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 
 public class TabFragment1 extends Fragment {
 
@@ -21,8 +25,9 @@ public class TabFragment1 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_tab_fragment1, container, false);
     }
+
     @Override
-    public void onActivityCreated(Bundle savedInstanceState){
+    public void onActivityCreated(Bundle savedInstanceState) {
         final ListView listView = (ListView) getView().findViewById(R.id.listView);
 
         super.onCreate(savedInstanceState);
@@ -39,14 +44,50 @@ public class TabFragment1 extends Fragment {
         new Firebase("https://crackling-fire-8381.firebaseio.com/todoItems")
                 .addChildEventListener(new ChildEventListener() {
                     public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                        adapter.add((String)dataSnapshot.child("text").getValue());
+                        adapter.add((String) dataSnapshot.child("text").getValue());
                     }
+
                     public void onChildRemoved(DataSnapshot dataSnapshot) {
-                        adapter.remove((String)dataSnapshot.child("text").getValue());
+                        adapter.remove((String) dataSnapshot.child("text").getValue());
                     }
-                    public void onChildChanged(DataSnapshot dataSnapshot, String s) { }
-                    public void onChildMoved(DataSnapshot dataSnapshot, String s) { }
-                    public void onCancelled(FirebaseError firebaseError) { }
+
+                    public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                    }
+
+                    public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+                    }
+
+                    public void onCancelled(FirebaseError firebaseError) {
+                    }
                 });
+        final EditText text = (EditText) getView().findViewById(R.id.todoText);
+        final Button button = (Button) getView().findViewById(R.id.addButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                new Firebase("https://crackling-fire-8381.firebaseio.com/todoItems")
+                        .push()
+                        .child("text")
+                        .setValue(text.getText().toString());
+            }
+        });
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                new Firebase("https://crackling-fire-8381.firebaseio.com/todoItems")
+                        .orderByChild("text")
+                        .equalTo((String) listView.getItemAtPosition(position))
+                        .addListenerForSingleValueEvent(new ValueEventListener() {
+                            public void onDataChange(DataSnapshot dataSnapshot) {
+                                if (dataSnapshot.hasChildren()) {
+                                    DataSnapshot firstChild = dataSnapshot.getChildren().iterator().next();
+                                    firstChild.getRef().removeValue();
+                                }
+                            }
+
+                            public void onCancelled(FirebaseError firebaseError) {
+                            }
+                        });
+            }
+        });
     }
 }
